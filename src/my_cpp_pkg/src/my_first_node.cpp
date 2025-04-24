62f5e77b71485c895be2582d5af7893a39843617
Jefferson Osowsky <jeff.osowsky@hotmail.com>
2025-04-24 10:07:43 -0300

    [ Chore ]: Write a C++ Node with OOP.

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,12 +1,35 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node
+{
+    public:
+        MyNode() : Node( "cpp_test" ), counter_( 0 )
+        {
+            RCLCPP_INFO( this->get_logger(), "Hello, world!" );
+            this->timer_ = this->create_wall_timer( std::chrono::seconds( 1 ),
+                                                    std::bind( &MyNode::timerCallback, this ) );
+        };
+
+    private:
+        void timerCallback()
+        {
+            RCLCPP_INFO( this->get_logger(), "Hello! [ %02d ]", this->counter_ );
+            this->counter_++;
+        };
+        rclcpp::TimerBase::SharedPtr timer_;
+        int counter_;
+};
+
 int main( int argc, char **argv )
 {
     rclcpp::init( argc, argv );
 
-    auto node = std::make_shared<rclcpp::Node>( "cpp_test" ); // Shared pointers.
-    RCLCPP_INFO( node->get_logger(), "Hello, world!" );
+    // No OOP.
+    // auto node = std::make_shared<rclcpp::Node>( "cpp_test" ); // Shared pointers.
+    // RCLCPP_INFO( node->get_logger(), "Hello, world!" );
+    auto node = std::make_shared<MyNode>(); // Shared pointers.
 
+    rclcpp::spin( node ); // keep the node alive.
     rclcpp::shutdown();
     return 0;
 };
